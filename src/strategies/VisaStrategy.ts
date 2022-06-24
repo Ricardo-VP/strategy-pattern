420f55e08ed26ff4ec49748ff30fc0339770b8cb
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-24 00:05:28 -0500

    Adding the visa payment strategy

---
+++ src/strategies/VisaStrategy.ts
@@ -0,0 +1,13 @@
+import { PaymentResponse } from "../interfaces/PaymentStrategy";
+import { CardPaymentStrategy } from "../interfaces/strategies/CardPaymentStrategy";
+
+export class VisaStrategy implements CardPaymentStrategy {
+  constructor(public cardNumber: string, public cvv: string) {}
+
+  execute(userId: string, amount: number): PaymentResponse {
+    console.log(
+      `Visa approved the transaction for ${userId} in the amount of ${amount}.`
+    );
+    return { isSuccess: true };
+  }
+}
